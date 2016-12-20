61e937068fcd08d3d062091bf68864083db1184d
Tapani Kiiskinen <tapani.kiiskinen@gmail.com>
2016-12-20 12:08:07 +0200

    Made install.py work from any directory.

--- install.py
+++ install.py
@@ -6,6 +6,7 @@ import glob
 import subprocess
 from itertools import chain
 
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 cwd = os.getcwd()
 if not cwd.endswith("config"):
     print("Not in the right folder! Has to be named config")
@@ -16,4 +17,3 @@ for filename in chain(glob.iglob("*"), glob.iglob(".*")):
     if filename in (".git", ".gitignore", "install.py"):
         continue
     subprocess.run(["ln", "-r", "-s", filename, os.path.expanduser("~/")])
-    print("Linked {0} to ~/{0}.".format(filename))
