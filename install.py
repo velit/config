f915abfa4ef320423c381895d35cddf3e6c79adc
Tapani Kiiskinen <tapani.kiiskinen@gmail.com>
2017-05-17 10:44:53 +0300

    Re-added install script and added local config creation

---
+++ install.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+import os
+import subprocess
+from pathlib import Path
+
+
+def make_folders():
+    for path in Path(".").glob("folders/*"):
+        if path.is_dir():
+            os.makedirs(Path.home() / path.name, exist_ok=True)
+
+
+local_gitconfig_content = (
+"""[user]
+    name = Tapani Kiiskinen
+    email = """
+)
+
+
+def write_local_settings():
+
+    local_gitconfig = Path.home() / Path(".gitconfig_local")
+    if not local_gitconfig.exists():
+        email = input("Input email for git identity: ")
+        with local_gitconfig.open('w') as f:
+            f.write(f"{local_gitconfig_content}{email}\n")
+
+    local_zsh_config = Path.home() / Path(".zsh_local")
+    if not local_zsh_config.exists():
+        with local_zsh_config.open('w') as f:
+            f.write()
+
+
+def stow(packages):
+    if not packages:
+        packages = [path.name for path in Path(".").glob("*/") if path.is_dir() and path.name[0] != '.']
+
+    home_path = str(Path.home())
+    subprocess.run(["stow", f"--target={home_path}"] + packages, stdout=subprocess.PIPE)
+
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(description="Make folders in userdir, write local settings"
+                                     " and stow given packages, stow all packages if none are given.")
+    parser.add_argument('packages', nargs='*')
+    args = parser.parse_args()
+    make_folders()
+    write_local_settings()
+    stow(args.packages)
