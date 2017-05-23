840346dab5bfe8fac5b7819874d75b6d6c32605f
Tapani Kiiskinen <tapani.kiiskinen@gmail.com>
2017-05-23 10:35:00 +0300

    Added web install command comment into install_bare.sh

--- install_bare.sh
+++ install_bare.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+# sh <(curl -s https://raw.githubusercontent.com/velit/config/master/install_bare.sh)
+
 # Install config files without setting up bin/links folders, without symlinks
 # or leaving a modifiable git repository behind
-wget -qO master.zip https://github.com/velit/config/archive/master.zip
+wget -q -O master.zip https://github.com/velit/config/archive/master.zip
 unzip -qo master.zip
 cp -i config-master/dotfiles/.[!.]* ~
 cp -ri config-master/vim/.vim/ ~
